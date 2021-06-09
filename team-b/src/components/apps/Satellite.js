4f239f8b6e1786cc1c43d30d1e53777f71bb20b5
Jason smith <jasonsmith@Jasons-MacBook-Pro.local>
2021-06-09 08:20:40 -0400

    added css

--- team-b/src/components/apps/Satellite.js
+++ team-b/src/components/apps/Satellite.js
@@ -1,24 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import './Satellite.css';
 
 const baseURL = 'https://api.nasa.gov/planetary/earth/imagery';
 const key = 'v21c2j0TmMbMfVZZ7zkZxjJhuw274JYPkhUZr87c';
 const lon = -86.14;
 const lat = 39.79;
-const dim = 0.4;
+const dim = 0.22;
 //try dim at 1.25
 
 const Nasa = () => {
-    
-let url = `${baseURL}?lon=${lon}&lat=${lat}&dim=${dim}&api_key=${key}`;
-            
-return(
-        <div className="main">
-            
-                Nasa test
-                <img src={`${baseURL}?lon=${lon}&lat=${lat}&dim=${dim}&api_key=${key}`} />
-            
-        </div>
-    )
+
+        let url = `${baseURL}?lon=${lon}&lat=${lat}&dim=${dim}&api_key=${key}`;
+
+        return (
+                <div className="main">
+
+                        
+                        <img src={`${baseURL}?lon=${lon}&lat=${lat}&dim=${dim}&api_key=${key}`} />
+
+                </div>
+        )
 }
 
 export default Nasa;
